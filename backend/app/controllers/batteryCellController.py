e87b598a86f3cd6ba68d180141ce3f2e91409d91
Jonathan <Jonathan@iMac.local>
2022-07-19 16:18:29 -0400

    cleaning up battery cell controller

--- backend/app/controllers/batteryCellController.py
+++ backend/app/controllers/batteryCellController.py
@@ -22,66 +22,66 @@ limiter = Limiter(key_func=get_remote_address)
 
 
 @router.get("/", response_model=schemas.BatteryCellsManyOut)
-async def get_batteryCells(db: AsyncSession = Depends(get_db),
-                           current_user: int = Depends(
-                               oauth2.get_current_user),
-                           limit: Optional[int] = 10,
-                           search: Optional[str] = "",
-                           cathode: Optional[str] = "",
-                           anode: Optional[str] = "",
-                           type: Optional[str] = "",
-                           source: Optional[str] = "",
-                           page: Optional[int] = 1,
-                           skip: Optional[int] = 0):
+async def get_battery_cells(db: AsyncSession = Depends(get_db),
+                            current_user: int = Depends(
+        oauth2.get_current_user),
+        limit: Optional[int] = 10,
+        search: Optional[str] = "",
+        cathode: Optional[str] = "",
+        anode: Optional[str] = "",
+        type: Optional[str] = "",
+        source: Optional[str] = "",
+        page: Optional[int] = 1,
+        skip: Optional[int] = 0):
 
     query = await db.execute(select(models.Battery_Cells).where(
         models.Battery_Cells.owner_id == current_user.id))
 
-    all_batteryCells = query.scalars().all()
+    all_battery_cells = query.scalars().all()
 
     if search and search != None:
-        all_batteryCells = list(filter(lambda x: re.search(
-            search, x.cell_name_id), all_batteryCells))
+        all_battery_cells = list(filter(lambda x: re.search(
+            search, x.cell_name_id), all_battery_cells))
 
-    all_batteryCells = search_query_battery_cell(
-        cathode, 'cathode', all_batteryCells)
-    all_batteryCells = search_query_battery_cell(
-        anode, 'anode', all_batteryCells)
-    all_batteryCells = search_query_battery_cell(
-        type, 'type', all_batteryCells)
-    all_batteryCells = search_query_battery_cell(
-        source, 'source', all_batteryCells)
+    all_battery_cells = search_query_battery_cell(
+        cathode, 'cathode', all_battery_cells)
+    all_battery_cells = search_query_battery_cell(
+        anode, 'anode', all_battery_cells)
+    all_battery_cells = search_query_battery_cell(
+        type, 'type', all_battery_cells)
+    all_battery_cells = search_query_battery_cell(
+        source, 'source', all_battery_cells)
 
-    total_battery_cells = len(all_batteryCells)
+    total_battery_cells = len(all_battery_cells)
 
-    avg_capacity = calc_float_avg("capacity_ah", all_batteryCells)
+    avg_capacity = calc_float_avg("capacity_ah", all_battery_cells)
     avg_depth_of_discharge = calc_float_avg(
-        "depth_of_discharge", all_batteryCells)
-    avg_temperature_c = calc_float_avg("temperature_c", all_batteryCells)
+        "depth_of_discharge", all_battery_cells)
+    avg_temperature_c = calc_float_avg("temperature_c", all_battery_cells)
 
     total_cathode_lco_cells = get_total_cells_by_value(
-        'cathode', 'LCO', all_batteryCells)
+        'cathode', 'LCO', all_battery_cells)
     total_cathode_lfp_cells = get_total_cells_by_value(
-        'cathode', 'LFP', all_batteryCells)
+        'cathode', 'LFP', all_battery_cells)
     total_cathode_nca_cells = get_total_cells_by_value(
-        'cathode', 'NCA', all_batteryCells)
+        'cathode', 'NCA', all_battery_cells)
     total_cathode_nmc_cells = get_total_cells_by_value(
-        'cathode', 'NMC', all_batteryCells)
+        'cathode', 'NMC', all_battery_cells)
     total_cathode_nmclco_cells = get_total_cells_by_value(
-        'cathode', 'NMC-LCO', all_batteryCells)
+        'cathode', 'NMC-LCO', all_battery_cells)
 
     avg_cycles_lco_cells = get_avg_attr_by_another_attr_value(
-        "cycles", "cathode", "LCO", all_batteryCells)
+        "cycles", "cathode", "LCO", all_battery_cells)
     avg_cycles_lfp_cells = get_avg_attr_by_another_attr_value(
-        "cycles", "cathode", "LFP", all_batteryCells)
+        "cycles", "cathode", "LFP", all_battery_cells)
     avg_cycles_nca_cells = get_avg_attr_by_another_attr_value(
-        "cycles", "cathode", "NCA", all_batteryCells)
+        "cycles", "cathode", "NCA", all_battery_cells)
     avg_cycles_nmc_cells = get_avg_attr_by_another_attr_value(
-        "cycles", "cathode", "NMC", all_batteryCells)
+        "cycles", "cathode", "NMC", all_battery_cells)
     avg_cycles_nmclco_cells = get_avg_attr_by_another_attr_value(
-        "cycles", "cathode", "NMC-LCO", all_batteryCells)
+        "cycles", "cathode", "NMC-LCO", all_battery_cells)
 
-    return {"battery_cells": all_batteryCells,
+    return {"battery_cells": all_battery_cells,
             "total_battery_cells": total_battery_cells,
             "avg_capacity": avg_capacity,
             "avg_depth_of_discharge": avg_depth_of_discharge,
@@ -101,71 +101,71 @@ async def get_batteryCells(db: AsyncSession = Depends(get_db),
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.BatteryCellOut)
 @limiter.limit("30/minute", error_message="Too many requests, please try again later")
-async def create_batteryCell(batteryCell: schemas.BatteryCellCreate,
-                             request: Request,
-                             db: AsyncSession = Depends(get_db),
-                             current_user: int = Depends(oauth2.get_current_user)):
+async def create_battery_cell(battery_cell: schemas.BatteryCellCreate,
+                              request: Request,
+                              db: AsyncSession = Depends(get_db),
+                              current_user: int = Depends(oauth2.get_current_user)):
 
     # make sure the response_model = the table itself for pytest
     # make sure in the function it says "request: Request" and not "req: Request", or else the slowapi rate limiter won't work
 
-    created_batteryCell = models.Battery_Cells(
-        **batteryCell.dict(), owner_id=current_user.id)
+    created_battery_cell = models.Battery_Cells(
+        **battery_cell.dict(), owner_id=current_user.id)
 
-    db.add(created_batteryCell)
+    db.add(created_battery_cell)
 
     await db.commit()
-    await db.refresh(created_batteryCell)
-    return created_batteryCell
+    await db.refresh(created_battery_cell)
+    return created_battery_cell
 
 
 @router.patch("/{id}", response_model=schemas.BatteryCellOut)
 @limiter.limit("10/minute", error_message="Too many requests, please try again later")
-async def update_batteryCell(request: Request,
-                             id: int,
-                             updating_batteryCell: schemas.BatteryCellUpdate,
-                             db: AsyncSession = Depends(get_db),
-                             current_user: int = Depends(oauth2.get_current_user)):
+async def update_battery_cell(request: Request,
+                              id: int,
+                              updating_battery_cell: schemas.BatteryCellUpdate,
+                              db: AsyncSession = Depends(get_db),
+                              current_user: int = Depends(oauth2.get_current_user)):
 
-    batteryCell = await db.get(models.Battery_Cells, id)
+    battery_cell = await db.get(models.Battery_Cells, id)
 
-    if not batteryCell:
+    if not battery_cell:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Battery cell with id: {id} does not exist")
 
-    if batteryCell.owner_id != current_user.id:
+    if battery_cell.owner_id != current_user.id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="Not authorized to perform requested action")
 
-    batteryCell_data = updating_batteryCell.dict(exclude_unset=True)
-    for key, value in batteryCell_data.items():
-        setattr(batteryCell, key, value)
+    battery_cell_data = updating_battery_cell.dict(exclude_unset=True)
+    for key, value in battery_cell_data.items():
+        setattr(battery_cell, key, value)
 
-    db.add(batteryCell)
+    db.add(battery_cell)
     await db.commit()
-    await db.refresh(batteryCell)
+    await db.refresh(battery_cell)
 
-    return batteryCell
+    return battery_cell
 
 
 @router.delete("/{id}", status_code=status.HTTP_200_OK)
 @limiter.limit("10/minute", error_message="Too many requests, please try again later")
-async def delete_batteryCell(request: Request,
-                             id: int,
-                             db: AsyncSession = Depends(get_db),
-                             current_user: int = Depends(oauth2.get_current_user)):
+async def delete_battery_cell(request: Request,
+                              id: int,
+                              db: AsyncSession = Depends(get_db),
+                              current_user: int = Depends(oauth2.get_current_user)):
 
-    batteryCell = await db.get(models.Battery_Cells, id)
+    battery_cell = await db.get(models.Battery_Cells, id)
 
-    if not batteryCell:
+    if not battery_cell:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Battery cell with id: {id} does not exist")
 
-    if batteryCell.owner_id != current_user.id:
+    if battery_cell.owner_id != current_user.id:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
                             detail="Not authorized to perform requested action")
 
-    await db.delete(batteryCell)
+    await db.delete(battery_cell)
     await db.commit()
 
     # if the battery cell gets deleted, then the csv data associated with that same battery cell should be deleted
@@ -195,29 +195,3 @@ async def delete_batteryCell(request: Request,
         pass
 
     return {"msg": "Success! Battery cell removed", "id": id}
-
-
-# @router.get("/testing")
-# async def get_batteryCells(db: AsyncSession = Depends(get_db),
-#                            current_user: int = Depends(
-#                                oauth2.get_current_user),
-#                            limit: Optional[int] = 10,
-#                            search: Optional[str] = "",
-#                            cathode: Optional[str] = "",
-#                            anode: Optional[str] = "",
-#                            type: Optional[str] = "",
-#                            source: Optional[str] = "",
-#                            page: Optional[int] = 1,
-#                            skip: Optional[int] = 0):
-
-#     query = await db.execute(select(models.Battery_Cells).where(
-#         models.Battery_Cells.owner_id == current_user.id))
-
-#     all_batteryCells = query.fetchall()
-
-#     # res = [dict(battery_cell) for battery_cell in all_batteryCells]
-
-#     # print("res: ", res)
-#     # print("res[0]: ", res[0])
-
-#     return {"battery_cells": all_batteryCells}
